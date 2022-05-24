e32a47e64017a47675407186b3b4cba436f91448
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-05-24 18:35:02 +0600

    get user data

--- index.js
+++ index.js
@@ -10,31 +10,52 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohg5x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: "UnAuthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 async function run(){
     try{
         await client.connect();
         const toolsCollection = client.db('bikes-tools').collection('tools');
         const itemsCollection = client.db('bikes-tools').collection('items');
+        const usersCollection = client.db('bikes-tools').collection('users');
         app.get('/tools', async(req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
-        })
+        });
 
         app.get('/tools/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const tool = await toolsCollection.findOne(query);
             res.send(tool);
-        })
+        });
 
         app.get('/items', async(req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
-        })
+        });
+
+        app.get("/users", verifyJWT, async (req, res) => {
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        });
+      
 
 }
     finally{
