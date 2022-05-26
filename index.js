9a9ca8ef4595ec2f1c43733dce12ef026a50f0f2
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-05-26 12:02:01 +0600

    get all users

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ async function run(){
             res.send(users);
         });
 
+        app.get('/admin/:email', async(req, res) => {
+          const email = req.params.email;
+          const user = await usersCollection.findOne({email: email});
+          const isAdmin = user.role === 'admin';
+          res.send({admin: isAdmin});
+      })
+
         app.put("/users/:email", async (req, res) => {
           const email = req.params.email;
           const user = req.body;
